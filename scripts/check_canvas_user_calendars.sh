9647f62385b851f5a4b4b5f6dc1ceba889589866
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2020-03-31 11:23:40 +0200

    Update canvas user calendars check script
    To catch duplicated user calendar events and overlapping events

--- scripts/check_canvas_user_calendars.sh
+++ scripts/check_canvas_user_calendars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 0 50 1800); do
+for i in $(seq 0 33 1400); do
     sed -i s/start\ =.*,/start\ =\ $i,/ check_canvas_user_calendars.apex
     sfdx force:apex:execute -f check_canvas_user_calendars.apex --loglevel INFO | tee output/check_canvas_user_calendars.out.$i | grep \|INFO\|
 done
