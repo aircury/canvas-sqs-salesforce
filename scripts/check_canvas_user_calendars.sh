8f23a236237496a11263e351a8e86c5f0f3f4337
Pedro Peña Pérez <pedro.pena@open-phoenix.com>
2021-02-23 11:58:33 +0100

    Some fixes for the checker process

--- scripts/check_canvas_user_calendars.sh
+++ scripts/check_canvas_user_calendars.sh
@@ -7,6 +7,8 @@ output_dir=output/$(date +"%Y_%m_%d")
 attendees=$(sfdx force:apex:execute --targetusername production -f count_canvas_user_calendars.apex --loglevel INFO | grep \|INFO\| | cut -d ":" -f 4)
 
 for i in $(seq 0 33 $attendees); do
+    echo "Batch $i"
     sed -i s/start\ =.*,/start\ =\ $i,/ check_canvas_user_calendars.apex
     sfdx force:apex:execute --targetusername production -f check_canvas_user_calendars.apex --loglevel INFO | tee $output_dir/check_canvas_user_calendars.out.$i | grep \|INFO\| | cut -d "|" -f 5
+    grep -q Error $output_dir/check_canvas_user_calendars.out.$i && echo "Error in batch $i"
 done
--- scripts/check_canvas_users.sh
+++ scripts/check_canvas_users.sh
@@ -6,7 +6,9 @@ output_dir=output/$(date +"%Y_%m_%d")
 
 participants=$(sfdx force:apex:execute --targetusername production -f count_canvas_users.apex --loglevel INFO | grep \|INFO\| | cut -d ":" -f 4)
 
-for i in $(seq 0 100 $participants); do
+for i in $(seq 0 33 $participants); do
+    echo "Batch $i"
     sed -i s/start\ =.*,/start\ =\ $i,/ check_canvas_users.apex
     sfdx force:apex:execute --targetusername production -f check_canvas_users.apex --loglevel INFO | tee $output_dir/check_canvas_users.out.$i | grep \|INFO\| | cut -d "|" -f 5
+    grep -q Error $output_dir/check_canvas_users.out.$i && echo "Error in batch $i"
 done
